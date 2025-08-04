3b3a47cc517c289d122b42e6050cf55154da126c
Zack <zackwritescode@gmail.com>
2025-08-03 21:17:59 -0400

    Update relay_logic.py
    include randomized LDs and a finalization option.

--- relay_logic.py
+++ relay_logic.py
@@ -1,29 +1,46 @@
-LOCATIONS = [
-    "Tucker", "Jamestown", "Knoxville", "Oxford", "Goldsboro", "Norfolk",
-    "Lynchburg", "Villa Rica", "Hope Mills", "Beckley", "Bluefield", "Galax",
-    "Gastonia", "Wilksboro", "Hickory", "Morganton", "Statesville",
-    "Spartanburg", "Syvla", "Anderson", "Greenville", "Laurens",
-    "Asheville", "Hendersonville"
+from math import ceil
+import random
+
+# Trailer with its associated info (stack count, overload info, etc.)
+class Trailer:
+    def __init__(self, number, stacks, overload_from=None):
+        self.number = number
+        self.stacks = stacks
+        self.overload_from = overload_from
+        self.ld_number = random.randint(100000000, 999999999)
+        self.trailer_number = ""
+        self.seal_number = ""
+# Shipping location and stack/tray calculations
 class Location:
-    def __int__(self, name, bread_trays, bulk_trays, cake_pallets):
+    def __init__(self, name, bread_trays, bulk_trays, cake_pallets):
         self.name = name
         self.bread_trays = bread_trays
         self.bulk_trays = bulk_trays
         self.cake_pallets = cake_pallets
-        self.total_stacks = 0
+        self.total_bread_stacks = ceil(bread_trays / 17)
+        self.total_bulk_stacks = ceil(bulk_trays / 30)
+        self.total_stacks = self.total_bread_stacks + self.total_bulk_stacks
         self.trailers = []
-    def calculate_total_stacks(self):
-        bread_stacks = -(-self.bread_trays // 17)
-        bulk_stacks = -(-self.bulk_trays // 30)
-        self.total_stacks = bread_stacks + bulk_stacks
-        return self.total_stacks
+    # Assign trailers based on stack count and cake pallets
     def assign_trailers(self):
-        total_for_distribution = self.total_stacks + (self.cake_pallets * 4)
-        stacks_remaining = total_for_distribution
+        stacks_remaining = self.total_stacks + (self.cake_pallets * 4)
         trailer_number = 1
         while stacks_remaining > 0:
-            stacks = min(stacks_remaining, 98)
-            self.trailers.append((f"{self.name} #{trailer_number}", stacks))
-            stacks_remaining -= stacks
+            count = min(stacks_remaining, 98)
+            self.trailers.append(Trailer(trailer_number, count))
+            stacks_remaining -= count
             trailer_number += 1
-
+    # Placeholder for FirstFleet tray information
+    def finalize_trailers(self):
+        for trailer in self.trailers:
+            trailer.bread_trays = None
+            trailer.bulk_trays = None
+    # Display relay information
+    def display_relay(self):
+        for trailer in self.trailers:
+            # If trailer has an overload, output overload info
+            overload_text = f"- Overload [{trailer.overload_from[0]} {trailer.overload_from[1]} stacks]" if trailer overload from else ""
+            # Print trailer details (LD number, stack count)
+            print(f"{self.name}:\nLD {trailer.ld_number} - Trailer #{trailer.number}: {trailer.stacks} stacks {overload_text}")
+            # Print empty input fields for trailer number and seal number
+            print(f" Trailer #: [ {trailer.trailer_number} ] Seal #: [ {trailer.seal_number} ]")
